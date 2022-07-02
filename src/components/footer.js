84541df316777c5ffc13d61e6843b05625d10c86
Will Downey <willd20@vt.edu>
2022-07-02 16:13:29 -0400

    Update footer content"

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,13 +2,15 @@
 
 function buildFooter() {
     let footer = document.createElement('footer');
-    let footerText = document.createElement('a');
+    let footerText = document.createElement('p');
     footerText.innerHTML = '© 2022 wrdowney';
-    footerText.href = 'github.com/wrdowney';
     footer.appendChild(footerText);
+    let footerLink = document.createElement('a');
+    footerLink.href = 'https://github.com/wrdowney'
     let footerImg = document.createElement('img');
+    footerLink.appendChild(footerImg);
     footerImg.src = '/images/github.svg';
-    footer.appendChild(footerImg);
+    footer.appendChild(footerLink);
 
     return footer;
 }
