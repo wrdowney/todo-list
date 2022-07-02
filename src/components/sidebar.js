bd44af370d7d07e9f61ebfcb95997865ddb5bfa9
Will Downey <willd20@vt.edu>
2022-07-02 12:48:39 -0400

    Sidebar desktop styling

---
+++ src/components/sidebar.js
@@ -0,0 +1,50 @@
+//build sidebar
+
+function buildSidebar() {
+    var sidebar = document.createElement('div');
+    sidebar.classList.add("sidebar");
+    sidebar.classList.add("active");
+    
+    let todayContainer = document.createElement('div');
+    todayContainer.classList.add("sidebar-container");
+    let todayImg = document.createElement('img');
+    todayImg.src = '/images/today.svg';
+    let todayText = document.createElement('p');
+    todayText.innerHTML = 'Today';
+    todayContainer.appendChild(todayImg);
+    todayContainer.appendChild(todayText);
+
+    let weekContainer = document.createElement('div');
+    weekContainer.classList.add("sidebar-container");
+    let weekImg = document.createElement('img');
+    weekImg.src = '/images/week.svg';
+    let weekText = document.createElement('p');
+    weekText.innerHTML = 'Week';
+    weekContainer.appendChild(weekImg);
+    weekContainer.appendChild(weekText);
+
+    let projects = document.createElement('div');
+    projects.classList.add("project-container");
+    let projectTitle = document.createElement('div');
+    projectTitle.innerHTML = "Projects";
+    let addProject = document.createElement("div");
+    addProject.classList.add("sidebar-container");
+    let addProjectImg = document.createElement("img")
+    addProjectImg.src = "/images/plus.svg";
+    let addProjectText = document.createElement("p");
+    addProjectText.innerHTML = "Add Project";
+    addProject.appendChild(addProjectImg);
+    addProject.appendChild(addProjectText);
+    projects.appendChild(projectTitle);
+    projects.appendChild(addProject);
+
+
+    sidebar.appendChild(todayContainer);
+    sidebar.appendChild(weekContainer);
+    sidebar.appendChild(projects);
+
+
+    return sidebar;
+}
+
+export {buildSidebar};
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,16 @@
 import { buildHeader } from "../components/header";
+import { buildSidebar} from "../components/sidebar";
 
 function loadHome() {
-    let content = document.getElementById('content');
+    let content = document.querySelector("body");
     content.appendChild(buildHeader());
+    let main = document.createElement('div');
+    main.classList.add("main");
+    main.appendChild(buildSidebar());
+    let currentProject = document.createElement('div');
+    currentProject.classList.add("current-project");
+    main.appendChild(currentProject);
+    content.appendChild(main);
 }
 
 export { loadHome };
