0adb77ebcb9b2452cb66561de1fc9851c6fdb6f2
Will Downey <willd20@vt.edu>
2022-07-02 16:52:33 -0400

    Storage of current projects

--- src/website.js
+++ src/website.js
@@ -1,12 +1,18 @@
 //builds default home page of website
+import Project from './data/project.js';
 import {loadHome} from './pages/home.js';
+let projects = [];
 
 function initializeWebsite() {
     loadHome();
 }
 
 function createProject(name) {
-    alert('testing');
+    projects.append(new Project(name));
+}
+
+function getProjects() {
+    return projects;
 }
 
 export {initializeWebsite, createProject};
